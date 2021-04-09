379e14d69cf361a888e36ab35ac7d05bfc25d6f0
amkashyap1220 <Alexander.kashyap@gmail.com>
2021-04-09 17:12:19 -0500

    finished assign 9

--- Assign9/tcp-z1926618.cc
+++ Assign9/tcp-z1926618.cc
@@ -1,4 +1,10 @@
-<<<<<<< HEAD
+/************************************************************
+ * @file tcp-z1926618.cc
+ * @author Alexander Kashyap (z1926618@students.niu.edu)
+ * @version Assign 9
+ * @date 2021-04-09
+ * CSCI 330 section 1
+ ************************************************************/
 #include <sys/types.h> /* See NOTES */
 #include <dirent.h>
 #include <sys/socket.h>
@@ -14,6 +20,10 @@
 
 using namespace std;
 
+/************************************************************
+ * Removes trailing newline or carriage return from string s
+ * @param s 
+ ************************************************************/
 void chomp(char *s)
 {
 	for (char *p = s + strlen(s) - 1; // start at the end of string
@@ -24,17 +34,21 @@ void chomp(char *s)
 
 int main(int argc, char *argv[])
 {
+	// The user MUST specify port and directory
 	if (argc != 3)
 	{
 		cerr << "error usage: ./z1918266 port directory" << endl;
 		exit(1);
 	}
 	char buffer[257];
+    char path[256];
 	socklen_t serverlen, clientlen;
 	ssize_t received;
 	int sock, newSock;
 	struct sockaddr_in echoserver; // structure for the address of the server
 	struct sockaddr_in echoclient;
+	struct stat s;
+
 	// Create the TCP socket
 	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
 	{
@@ -49,8 +63,7 @@ int main(int argc, char *argv[])
 	echoserver.sin_port = htons(atoi(argv[1])); // Server Port
 
 	// Bind the socket
-	serverlen = sizeof(echoserver);
-	if (bind(sock, (struct sockaddr *)&echoserver, serverlen) < 0)
+	if (bind(sock, (struct sockaddr *)&echoserver, sizeof(echoserver)) < 0)
 	{
 		perror("failed to bind server socket");
 		exit(1);
@@ -62,112 +75,62 @@ int main(int argc, char *argv[])
 		perror("failure in listen");
 		exit(1);
 	}
-	struct stat s;
+
 	if (stat(argv[2], &s) == -1)
 	{
 		perror("Error in stat");
 		exit(1);
 	}
-	if ((s.st_mode & S_IFMT) != S_IFDIR)
+
+	if (!S_ISDIR(s.st_mode))
 	{
 		perror("Argument must be directory");
 		exit(1);
 	}
-	char path[256];
 
-	// Run until cancelled
-	clientlen = sizeof(echoclient);
-	while (int newSock = accept(sock, (struct sockaddr *)&echoclient, &clientlen) != -1)
+    clientlen = sizeof(echoclient);
+	newSock = accept(sock, (struct sockaddr *)&echoclient, &clientlen);
+	struct stat path_s;
+	// Fork to handle the client (now child)
+	int pid = fork();
+	if (pid == 0)
 	{
-		int pid = fork();
-		if (pid == 0)
+		cout << "Client connected: " << inet_ntoa(echoclient.sin_addr) << endl;
+		char path[256];
+		if ((received = read(newSock, path, sizeof(path))) < 0)
 		{
-			cerr << "Client connected: " << inet_ntoa(echoclient.sin_addr) << endl;
-			char path[256];
-			if ((received = read(newSock, path, sizeof(path))) < 0)
+			perror("Failed to reveive message");
+			exit(1);
+		}
+		// Cleaning and formating
+		path[received] = '\0';
+		chomp(path);
+		string st(path);
+		char *pathname = strcat(argv[2], st.substr(st.find("/")).c_str());
+
+		// cout << pathname << endl;
+
+		// Check if it exist
+		if (stat(pathname, &path_s) != -1)
+		{
+			// Now, is it a dir?
+			if (S_ISDIR(path_s.st_mode))
 			{
-				perror("Failed to reveive message");
-				exit(1);
-			}
-			chomp(path);
-			string st(path);
-			if (stat(strcat(argv[2], st.c_str()), &s) == 0)
-			{
-				if (s.st_mode & S_IFDIR)
+				// Does this dir contain index.html?
+				struct stat indexcheck;
+				char buf[1024];
+				strcpy(buf, pathname);
+				strcat(buf, "/index.html");
+				if (stat(buf, &indexcheck) != -1)
 				{
-					struct stat indexcheck;
-					char buf[1024];
-					strcpy(buf, argv[2]);
-					strcat(buf, st.c_str());
-					strcat(buf, "index.html");
-					if (stat(buf, &indexcheck) == 0)
-					{
-						int fd;
-						if ((fd = open(buf, O_RDONLY, 0644)) == -1)
-						{
-							perror("open");
-							exit(1);
-						}
-
-						close(fd);
-						int number_read;
-						while ((number_read = read(fd, buffer, 256)) != 0)
-						{
-							if (number_read == -1)
-							{
-								perror("read");
-								exit(1);
-							}
-
-							if (write(newSock, buffer, number_read) == -1)
-							{
-								perror("write");
-								exit(1);
-							}
-						}
-					}
-					else
-					{
-						DIR *dirp = opendir(strcat(argv[2], st.c_str()));
-						if (dirp == 0)
-						{
-							perror("error opening dir: ");
-							exit(1);
-						}
-
-						// read directory entries
-
-						struct dirent *dirEntry;
-						while ((dirEntry = readdir(dirp)) != NULL)
-						{
-
-							if (dirEntry->d_name[0] != '.')
-							{
-								strcpy(buffer, dirEntry->d_name);
-								strcat(buffer, "\n");
-								if (write(newSock, buffer, strlen(buffer)) < 0)
-								{
-									perror("error in write: ");
-									exit(1);
-								}
-							}
-						}
-
-						closedir(dirp);
-						close(newSock);
-						exit(0);
-					}
-				}
-				else if (s.st_mode & S_IFREG)
-				{
+					// Print out contents of index.html
 					int fd;
-					if ((fd = open(strcat(argv[2], st.c_str()), O_RDONLY, 0644)) == -1)
+					if ((fd = open(buf, O_RDONLY, 0644)) == -1)
 					{
 						perror("open");
 						exit(1);
 					}
 
-					close(fd);
 					int number_read;
 					while ((number_read = read(fd, buffer, 256)) != 0)
 					{
@@ -176,34 +139,88 @@ int main(int argc, char *argv[])
 							perror("read");
 							exit(1);
 						}
-
 						if (write(newSock, buffer, number_read) == -1)
 						{
 							perror("write");
 							exit(1);
 						}
 					}
+					cout << endl;
+					close(fd);
+				}
+				else
+				{
+					// print everything from the dir
+					DIR *dirp = opendir(pathname);
+					if (dirp == 0)
+					{
+						perror("error opening dir: ");
+						exit(1);
+					}
+
+					// Getting all directory entry
+					struct dirent *dirEntry;
+					while ((dirEntry = readdir(dirp)) != NULL)
+					{
+
+						if (dirEntry->d_name[0] != '.')
+						{
+							strcpy(buffer, dirEntry->d_name);
+							strcat(buffer, ", ");
+							if (write(newSock, buffer, strlen(buffer)) < 0)
+							{
+								perror("error in write: ");
+								exit(1);
+							}
+						}
+					}
+
+					closedir(dirp);
+					close(newSock);
+					exit(0);
 				}
 			}
-			else
+			else if (S_ISREG(path_s.st_mode))
 			{
-				perror("file not found");
-				exit(1);
+				// this is a reg file
+				// print the contents of the regular file
+				int fd;
+				if ((fd = open(pathname, O_RDONLY, 0644)) == -1)
+				{
+					perror("open");
+					exit(1);
+				}
+
+				int number_read;
+				while ((number_read = read(fd, buffer, 256)) != 0)
+				{
+					if (number_read == -1)
+					{
+						perror("read");
+						exit(1);
+					}
+
+					if (write(newSock, buffer, number_read) == -1)
+					{
+						perror("write");
+						exit(1);
+					}
+				}
+				close(fd);
 			}
 		}
 		else
 		{
-			close(newSock);
+			// file/dir doesnt exist
+			perror("file not found");
+			exit(1);
 		}
 	}
+	else
+	{
+		// Parent
+		close(newSock);
+	}
+	// close connection
 	close(sock);
 }
-=======
-
-using namespace std;
-
-int main(int argc, char * argv[])
-{
-
-}
->>>>>>> 1ddff558acc079fa63030a4426d8b99d2c9575cd
